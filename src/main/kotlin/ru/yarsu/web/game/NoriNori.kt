7af0e6fe9e5311b89e3ac4468017dff487ca2adb
pakazakovyar <pakazakovyar@yandex.ru>
2025-02-15 23:27:41 +0300

    алгоритм расстановки блоков

--- src/main/kotlin/ru/yarsu/web/game/NoriNori.kt
+++ src/main/kotlin/ru/yarsu/web/game/NoriNori.kt
@@ -3,30 +3,154 @@ package ru.yarsu.web.game
 import kotlin.random.Random
 
 class NoriNori(private val fieldHeight: Int, private val fieldWith: Int) {
-    var fieldWithBlocks = List(fieldHeight) { List(fieldWith) { 0 } }
-    var fieldWithBorders = List(fieldHeight) { List(fieldWith) { 0 } }
+    var fieldWithBlocks = List(fieldHeight) { MutableList(fieldWith) { 0 } }
+    var fieldWithBorders = List(fieldHeight) { MutableList(fieldWith) { 0 } }
 
     override fun toString(): String {
-        return fieldWithBlocks.toString()
+        var res = ""
+        fieldWithBlocks.forEach {
+            res += it.toString() + "\n"
+        }
+        res += "\n"
+        fieldWithBorders.forEach {
+            res += it.toString() + "\n"
+        }
+        return res
     }
 
     fun generateBlocks() {
-        var attempts = fieldWith * fieldHeight
+        var attempts = fieldWith * fieldHeight * 1000
         while (attempts-- != 0) {
+
             val targetX = Random.nextInt(fieldWith)
             val targetY = Random.nextInt(fieldHeight)
-        }
-        fun isValidBlock(x: Int, y: Int): Boolean {
-            if (x !in 0 until fieldWith) return false;
-            else if (y !in 0 until fieldHeight) return false
-            else if (fieldWithBlocks[x][y] != 0) return false
 
-            if (x == 0) {
-                if (y == 0 && (fieldWithBlocks[x + 1][y] != 0 || fieldWithBlocks[x][y + 1] != 0)) return false
-                else if (y == fieldHeight - 1 && (fieldWithBlocks[x + 1][y] != 0 || fieldWithBlocks[x][y - 1] != 0)) return false
+            if (isValidBlock(targetX, targetY)) {
+
+                if (targetX == 0 && targetY == 0) {
+                    if (isValidBlock(targetX, targetY + 1)) {
+                        fieldWithBlocks[targetY][targetX] = 1
+                        fieldWithBlocks[targetY + 1][targetX] = 1
+                    } else if (isValidBlock(targetX + 1, targetY)) {
+                        fieldWithBlocks[targetY][targetX] = 1
+                        fieldWithBlocks[targetY][targetX + 1] = 1
+                    }
+                } else if (targetX == fieldWith - 1 && targetY == 0) {
+                    if (isValidBlock(targetX, targetY + 1)) {
+                        fieldWithBlocks[targetY][targetX] = 1
+                        fieldWithBlocks[targetY + 1][targetX] = 1
+                    } else if (isValidBlock(targetX - 1, targetY)) {
+                        fieldWithBlocks[targetY][targetX] = 1
+                        fieldWithBlocks[targetY][targetX - 1] = 1
+                    }
+                } else if (targetX == fieldWith - 1 && targetY == fieldHeight - 1) {
+                    if (isValidBlock(targetX - 1, targetY)) {
+                        fieldWithBlocks[targetY][targetX] = 1
+                        fieldWithBlocks[targetY][targetX - 1] = 1
+                    } else if (isValidBlock(targetX, targetY - 1)) {
+                        fieldWithBlocks[targetY][targetX] = 1
+                        fieldWithBlocks[targetY - 1][targetX] = 1
+                    }
+                } else if (targetX == 0 && targetY == fieldHeight - 1) {
+                    if (isValidBlock(targetX + 1, targetY)) {
+                        fieldWithBlocks[targetY][targetX] = 1
+                        fieldWithBlocks[targetY][targetX + 1] = 1
+                    } else if (isValidBlock(targetX, targetY - 1)) {
+                        fieldWithBlocks[targetY][targetX] = 1
+                        fieldWithBlocks[targetY - 1][targetX] = 1
+                    }
+                } else if (targetY == 0) {
+                    if (isValidBlock(targetX + 1, targetY)) {
+                        fieldWithBlocks[targetY][targetX] = 1
+                        fieldWithBlocks[targetY][targetX + 1] = 1
+                    } else if (isValidBlock(targetX - 1, targetY)) {
+                        fieldWithBlocks[targetY][targetX] = 1
+                        fieldWithBlocks[targetY][targetX - 1] = 1
+                    } else if (isValidBlock(targetX, targetY + 1)) {
+                        fieldWithBlocks[targetY][targetX] = 1
+                        fieldWithBlocks[targetY + 1][targetX] = 1
+                    }
+                } else if (targetY == fieldHeight - 1) {
+                    if (isValidBlock(targetX + 1, targetY)) {
+                        fieldWithBlocks[targetY][targetX] = 1
+                        fieldWithBlocks[targetY][targetX + 1] = 1
+                    } else if (isValidBlock(targetX - 1, targetY)) {
+                        fieldWithBlocks[targetY][targetX] = 1
+                        fieldWithBlocks[targetY][targetX - 1] = 1
+                    } else if (isValidBlock(targetX, targetY - 1)) {
+                        fieldWithBlocks[targetY][targetX] = 1
+                        fieldWithBlocks[targetY - 1][targetX] = 1
+                    }
+                } else if (targetX == 0) {
+                    if (isValidBlock(targetX + 1, targetY)) {
+                        fieldWithBlocks[targetY][targetX] = 1
+                        fieldWithBlocks[targetY][targetX + 1] = 1
+                    } else if (isValidBlock(targetX, targetY + 1)) {
+                        fieldWithBlocks[targetY][targetX] = 1
+                        fieldWithBlocks[targetY + 1][targetX] = 1
+                    } else if (isValidBlock(targetX, targetY - 1)) {
+                        fieldWithBlocks[targetY][targetX] = 1
+                        fieldWithBlocks[targetY - 1][targetX] = 1
+                    }
+                } else if (targetX == fieldWith - 1) {
+                    if (isValidBlock(targetX - 1, targetY)) {
+                        fieldWithBlocks[targetY][targetX] = 1
+                        fieldWithBlocks[targetY][targetX - 1] = 1
+                    } else if (isValidBlock(targetX, targetY + 1)) {
+                        fieldWithBlocks[targetY][targetX] = 1
+                        fieldWithBlocks[targetY + 1][targetX] = 1
+                    } else if (isValidBlock(targetX, targetY - 1)) {
+                        fieldWithBlocks[targetY][targetX] = 1
+                        fieldWithBlocks[targetY - 1][targetX] = 1
+                    }
+                } else {
+                    if (isValidBlock(targetX - 1, targetY)) {
+                        fieldWithBlocks[targetY][targetX] = 1
+                        fieldWithBlocks[targetY][targetX - 1] = 1
+                    } else if (isValidBlock(targetX, targetY + 1)) {
+                        fieldWithBlocks[targetY][targetX] = 1
+                        fieldWithBlocks[targetY + 1][targetX] = 1
+                    } else if (isValidBlock(targetX, targetY - 1)) {
+                        fieldWithBlocks[targetY][targetX] = 1
+                        fieldWithBlocks[targetY - 1][targetX] = 1
+                    } else if (isValidBlock(targetX + 1, targetY)) {
+                        fieldWithBlocks[targetY][targetX] = 1
+                        fieldWithBlocks[targetY][targetX + 1] = 1
+                    }
+                }
+
             }
-
-
         }
+
     }
+
+    fun isValidBlock(x: Int, y: Int): Boolean {
+        if (x !in 0 until fieldWith) return false;
+        else if (y !in 0 until fieldHeight) return false
+        else if (fieldWithBlocks[y][x] != 0) return false
+        if (x == 0 && y == 0)
+            return fieldWithBlocks[y + 1][x] == 0 && fieldWithBlocks[y][x + 1] == 0
+        else if (x == fieldWith - 1 && y == 0)
+            return fieldWithBlocks[y + 1][x] == 0 && fieldWithBlocks[y][x - 1] == 0
+        else if (x == fieldWith - 1 && y == fieldHeight - 1)
+            return fieldWithBlocks[y - 1][x] == 0 && fieldWithBlocks[y][x - 1] == 0
+        else if (x == 0 && y == fieldHeight - 1)
+            return fieldWithBlocks[y - 1][x] == 0 && fieldWithBlocks[y][x + 1] == 0
+        else if (y == 0)
+            return fieldWithBlocks[y + 1][x] == 0 && fieldWithBlocks[y][x + 1] == 0 && fieldWithBlocks[y][x - 1] == 0
+        else if (y == fieldHeight - 1)
+            return fieldWithBlocks[y - 1][x] == 0 && fieldWithBlocks[y][x + 1] == 0 && fieldWithBlocks[y][x - 1] == 0
+        else if (x == 0)
+            return fieldWithBlocks[y + 1][x] == 0 && fieldWithBlocks[y][x + 1] == 0 && fieldWithBlocks[y - 1][x] == 0
+        else if (x == fieldWith - 1)
+            return fieldWithBlocks[y + 1][x] == 0 && fieldWithBlocks[y][x - 1] == 0 && fieldWithBlocks[y - 1][x] == 0
+        else return fieldWithBlocks[y + 1][x] == 0 && fieldWithBlocks[y][x - 1] == 0 && fieldWithBlocks[y - 1][x] == 0 && fieldWithBlocks[y ][x + 1] == 0
+//        return true
+    }
+}
+
+fun main() {
+    var nori = NoriNori(8, 8)
+    nori.generateBlocks()
+    println(nori)
 }
